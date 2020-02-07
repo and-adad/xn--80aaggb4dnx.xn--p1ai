79af93959be3c64f1b5a6d0502f8df9b766e9c41
and-adad <56131670+and-adad@users.noreply.github.com>
2020-02-07 20:47:32 +0500

    Update db.php

--- db.php
+++ db.php
@@ -4,5 +4,6 @@ require "libs/rb.php";
 // этот файл подключает библиотеку rb.php.
 R::setup( 'mysql:host=localhost;dbname=nn.ru','root','');
 // Соединяем rb.php с БД. Предварительно я через PHPmyAdmin создал новую базу данных utf general ci
-
+R::freeze(TRUE);
+//этой командой я заморозил возможности red bean чтобы повысить производительность кода в продакшене
 session_start();
